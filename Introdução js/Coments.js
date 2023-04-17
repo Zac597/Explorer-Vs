67c0d19d2fd4f371fad012a7420bd5323acfa9b0
Zac597 <isaaclima597@gmail.com>
2023-04-17 16:52:11 -0300

    Start-js

---
+++ Introdução js/Coments.js
@@ -0,0 +1,10 @@
+// Aqui e tenho um coment
+//O código abaixo escreve alguma msg no devtools
+
+/*
+  ou posso fazer assim
+*/
+
+
+console.log('Bem vindos ao Starter') // Posso pegar no fim do coment
+console.log('Não irei ignorar')
---
+++ Introdução js/scripts.js
@@ -0,0 +1,37 @@
+/*  
+
+STRING
+  "" 
+  '' 
+  ``
+
+NUMBER
+  33 // inteiros
+  12.5 // reais
+  NaN // Not a number
+  Infinity // infinito
+
+Boolean
+  Verdeiro ou falso
+  True = verdadeiro
+  false = falso
+
+undefined
+  indefinido
+
+null
+  nulo
+  objeto que não possui nada dentro
+  diferente de indefinido
+
+Object
+  Objeto
+  propriedades / atributos
+  funcionalidades / Métodos
+
+Array (Vetores)
+  Uma lista
+  agrupamento de dados
+*/
+
+console.log(["Leite", "Ovos", 2 , 3])
