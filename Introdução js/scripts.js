ba94261bfeea9229f2c992b5fad9e11fa03679d4
Zac597 <isaaclima597@gmail.com>
2023-04-19 19:53:56 -0300

    Exercicios

--- Introdução js/scripts.js
+++ Introdução js/scripts.js
@@ -34,4 +34,75 @@ Array (Vetores)
   agrupamento de dados
 */
 
-console.log(["Leite", "Ovos", 2 , 3])
+// console.log(["Leite", "Ovos", 2 , 3])
+
+var name
+//assigment or atribuição de valores
+name = "Isaac"
+// console.log(typeof name)
+
+let age, isHuman
+
+age = 21
+isHuman = true
+
+// concatenação de valores
+
+  // console.log(name, age, isHuman)
+  // console.log('O ' + name + ' tem ' + age + ' anos')
+
+    // console.log(`O ${name} tem ${age} anos`)
+
+// Object 
+
+// const person = {
+//   name:'Jhon',
+//   age: 30,
+//   weight: 88.6,
+//   isAdmin: true
+// }
+
+// console.log(`${person.name} tem ${age} anos`)
+
+// Array 
+
+// const animals = [
+//   'Lion',
+//   'Monkey',
+//   {
+//     name:'cat',
+//     age: 3
+//   }
+// ]
+
+// console.log(animals[2].age)
+
+// ex
+
+let student = {
+  name : 'Isaac',
+  age : 21,
+  stars : 4.9,
+  isSubscribed : true
+}
+
+
+
+const students = [
+  student = [
+    student
+  ]
+]
+
+const john = {
+  name : "john",
+  age : 21,
+  weight : 86.9,
+}
+
+students[1] = john
+
+console.log(students[1])
+
+console.log(a)
+var a = 1
