ce3194e0260c8a921f22f17144335348f5bbc40c
Zac597 <isaaclima597@gmail.com>
2023-05-18 18:39:13 -0300

    desafio site

---
+++ projeto 09/main.js
@@ -0,0 +1,16 @@
+// 1  alert("Hello World!")
+/* 2
+numOne = prompt('Digite um número')
+numTwo = prompt('Digite outro número')
+alert(Number(numOne) + Number(numTwo))*/
+
+/* 3 
+num = prompt('Digite um número') 
+
+if(num != Number) {
+  alert('not')
+} else {
+  alert('é')
+}*/
+
+/* 
