54a545c2655e7370dfc5a245789d329f58ca6136
Zac597 <isaaclima597@gmail.com>
2023-06-15 06:11:43 -0300

    Focus timer dark mode

---
+++ Focus-timer-dark/scripts/elements.js
@@ -0,0 +1,34 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const Forest = document.querySelector('.buttonForest')
+const Rain = document.querySelector('.buttonRain')
+const Commerce = document.querySelector('.buttonCommerce')
+const Fire = document.querySelector('.buttonFire')
+const body = document.body
+const moreMinutes = document.querySelector('.moreMinutes')
+const anyLess = document.querySelector('.anyLess')
+
+export {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay,
+  Forest,
+  Rain,
+  Commerce,
+  Fire,
+  body,
+  moreMinutes,
+  anyLess
+
+}
